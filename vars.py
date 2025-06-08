3d7ff3739a80e64e90a7e83ddccd939db09b6593
#Vish <www.vishalsakariya1234@gmail.com>
2025-06-08 13:25:20 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,8 +6,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", "26738089"))
 API_HASH = environ.get("API_HASH", "5e28dcf17a1c58cfe7cf75d175f359a1")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "8109989096
-"))
+OWNER = int(environ.get("OWNER", "8109989096"))
 CREDIT = "VISH BOTS"
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
